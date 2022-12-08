fb1a1a0e47460e6a8dc5bf8a7c0b96147374a75c
Iceman8911 <wuchijss3@gmail.com>
2022-12-08 17:08:29 +0100

    Implemented Nameboxes using the Multichoice system as a base
    Implemented Nameboxes using the Multichoice system as a base
    
    This is the cleanest prototype so far
    
    still working
    
    Added Dummy Arguments,
    
    still testing
    
    Attached the command to the msgbox macro and replaced the newer one I made to save space,
    Fixed a few bugs,
    I also need to check how to stop it from glitching with other multichoices...
    
    ALMOST DONE
    
    DONE. IMPLEMENTED NAME BOXES.
    
    A little fix
    
    Added a delay
    
    FINALLY, the NameBox works without a hitch. PERFECT

--- include/script_menu.h
+++ include/script_menu.h
@@ -17,4 +17,10 @@ int ScriptMenu_AdjustLeftCoordFromWidth(int left, int width);
 bool16 ScriptMenu_CreatePCMultichoice(void);
 void ScriptMenu_DisplayPCStartupPrompt(void);
 
+void DrawObjEventNameBox(u8 left, u8 top, const u8 *text);
+extern u8 nameBoxWindowId; //used to clear the name box in field_message_box.c
+extern bool8 isNameBoxActive;
+extern u8 tempWindowId;
+extern const u8 *tempText;
+
 #endif //GUARD_SCRIPT_MENU_H
