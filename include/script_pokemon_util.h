4400c1e98ecd6084d6a0708fe9ce0bfea60db6a4
Iceman8911 <wuchijss3@gmail.com>
2023-04-05 14:50:09 +0100

    Another Goodra Buff, and some cleaning

--- include/script_pokemon_util.h
+++ include/script_pokemon_util.h
@@ -1,8 +1,8 @@
 #ifndef GUARD_SCRIPT_POKEMON_UTIL_H
 #define GUARD_SCRIPT_POKEMON_UTIL_H
 
-u8 ScriptGiveMon(u16, u8, u16, u32, u32, u8);
-u8 ScriptGiveEgg(u16);
+u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 unused3);
+u8 ScriptGiveEgg(u16 species);
 void CreateScriptedWildMon(u16, u8, u16);
 void CreateScriptedDoubleWildMon(u16, u8, u16, u16, u8, u16);
 void ScriptSetMonMoveSlot(u8, u16, u8);
